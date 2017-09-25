bc88caed6c57032e73f32fe30c5634b1ef6d39fb
Peter Mathis <petschki@users.noreply.github.com>
2017-09-25 13:07:19 +0200

    create sysv init script

---
+++ sysv-init.sh
@@ -0,0 +1,55 @@
+#! /bin/sh
+### BEGIN INIT INFO
+# Provides:          supervisor
+# Required-Start:    $remote_fs $syslog
+# Required-Stop:     $remote_fs $syslog
+# Default-Start:     2 3 4 5
+# Default-Stop:      0 1 6
+# Short-Description: supervisor init script
+# Description:       This file should be used to construct scripts to be
+#                    placed in /etc/init.d.
+### END INIT INFO
+
+# Author: Peter Mathis <peter.mathis@kombinat.at>
+#
+
+# Do NOT "set -e"
+
+# PATH should only include /usr/* if it runs after the mountnfs.sh script
+PATH=/sbin:/usr/sbin:/bin:/usr/bin
+HOME="/home/<user>/zope_buildout"
+DESC="Supervisor"
+NAME=supervisord
+DAEMON=$HOME/bin/$NAME
+CONTROL=$HOME/bin/supervisorctl
+DAEMON_ARGS=""
+PIDFILE=$HOME/var/$NAME.pid
+SCRIPTNAME=/etc/init.d/$NAME
+
+# Exit if the package is not installed
+[ -x "$DAEMON" ] || exit 0
+
+case "$1" in
+  start)
+	[ "$VERBOSE" != no ] && log_daemon_msg "Starting $DESC" "$NAME"
+	$DAEMON
+	;;
+  stop)
+	[ "$VERBOSE" != no ] && log_daemon_msg "Stopping $DESC" "$NAME"
+	$CONTROL shutdown
+	;;
+  status)
+	$CONTROL status
+	;;
+  restart|force-reload)
+	log_daemon_msg "Restarting $DESC" "$NAME"
+	$CONTROL reload
+	;;
+  *)
+	#echo "Usage: $SCRIPTNAME {start|stop|restart|reload|force-reload}" >&2
+	echo "Usage: $SCRIPTNAME {start|stop|status|restart|force-reload}" >&2
+	exit 3
+	;;
+esac
+
+:
