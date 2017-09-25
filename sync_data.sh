d7aad16732da28be96507e715db3a4f31ebdd9ac
Peter Mathis <peter.mathis@kombinat.at>
2017-09-25 16:23:15 +0200

    add ssh-keyscan

--- sync_data.sh
+++ sync_data.sh
@@ -10,6 +10,10 @@ REMOTE_PWD=$REPLY
 read -p "Remote Server: " -r
 HOST=$REPLY
 
+if [ -z "`cat /home/$USER/.ssh/known_hosts | grep $HOST`" ]; then
+    su - $USER -c "ssh-keyscan $HOST >> .ssh/known_hosts"
+fi
+
 su - $USER -c "mkdir -p stats && mkdir log"
 su - $USER -c "rsync -Prv --rsh 'sshpass -p $REMOTE_PWD ssh -l $REMOTE_USER' $REMOTE_USER@$HOST:/home/$REMOTE_USER/zope/buildout/var/filestorage/Data.fs ./zope_buildout/var/filestorage/"
 su - $USER -c "rsync -Prv --rsh 'sshpass -p $REMOTE_PWD ssh -l $REMOTE_USER' $REMOTE_USER@$HOST:/home/$REMOTE_USER/zope/buildout/var/blobstorage ./zope_buildout/var/"
