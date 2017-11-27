720dfb8de86da6c8eb5a10d80d0d95e239f1cb48
Peter Mathis <peter.mathis@kombinat.at>
2017-11-27 09:52:29 +0100

    read user subpath from userinpu

--- sync_data.sh
+++ sync_data.sh
@@ -9,13 +9,15 @@ read -p "Remote Password: " -r
 REMOTE_PWD=$REPLY
 read -p "Remote Server: " -r
 HOST=$REPLY
+read -p "Remote Subbath in User directory: " -r
+SUBPATH=$REPLY
 
 if [ -z "`cat /home/$USER/.ssh/known_hosts | grep $HOST`" ]; then
     su - $USER -c "ssh-keyscan $HOST >> .ssh/known_hosts"
 fi
 
 su - $USER -c "mkdir -p stats && mkdir log"
-su - $USER -c "rsync -Prv --rsh 'sshpass -p $REMOTE_PWD ssh -l $REMOTE_USER' $REMOTE_USER@$HOST:/home/$REMOTE_USER/zope/buildout/var/filestorage/Data.fs ./zope_buildout/var/filestorage/"
-su - $USER -c "rsync -Prv --rsh 'sshpass -p $REMOTE_PWD ssh -l $REMOTE_USER' $REMOTE_USER@$HOST:/home/$REMOTE_USER/zope/buildout/var/blobstorage ./zope_buildout/var/"
+su - $USER -c "rsync -Prv --rsh 'sshpass -p $REMOTE_PWD ssh -l $REMOTE_USER' $REMOTE_USER@$HOST:/home/$REMOTE_USER/$SUBPATH/var/filestorage/Data.fs ./zope_buildout/var/filestorage/"
+su - $USER -c "rsync -Prv --rsh 'sshpass -p $REMOTE_PWD ssh -l $REMOTE_USER' $REMOTE_USER@$HOST:/home/$REMOTE_USER/$SUBPATH/var/blobstorage ./zope_buildout/var/"
 su - $USER -c "rsync -Prv --rsh 'sshpass -p $REMOTE_PWD ssh -l $REMOTE_USER' $REMOTE_USER@$HOST:/home/$REMOTE_USER/log/* ./log/"
 su - $USER -c "rsync -Prv --rsh 'sshpass -p $REMOTE_PWD ssh -l $REMOTE_USER' $REMOTE_USER@$HOST:/home/$REMOTE_USER/stats/* ./stats/"
