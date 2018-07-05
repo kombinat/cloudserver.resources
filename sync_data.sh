8961657bce587eb7bd579d3510f37ee28e412156
Peter Mathis <peter.mathis@kombinat.at>
2018-07-05 12:04:35 +0200

    split remote login and remote account name

--- sync_data.sh
+++ sync_data.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 apt-get install -y sshpass
 
-read -p "Sync Stats/Logs/ZODB for Username: " -r
+read -p "Sync Stats/Logs/ZODB for local Username: " -r
 USER=$REPLY
-read -p "Remote Username: " -r
+read -p "Remote Server Login: " -r
 REMOTE_USER=$REPLY
-read -p "Remote Password: " -r
+read -p "Remote Server Password: " -r
 REMOTE_PWD=$REPLY
 read -p "Remote Server: " -r
 HOST=$REPLY
-read -p "Remote Subbath in User directory: " -r
+read -p "Remote Account (user in /home directory): " -r
+REMOTE_ACCOUNT=$REPLY
+read -p "Remote Subpath in User directory (eg. zope/buildout): " -r
 SUBPATH=$REPLY
 
 if [ -z "`cat /home/$USER/.ssh/known_hosts | grep $HOST`" ]; then
@@ -17,7 +19,7 @@ if [ -z "`cat /home/$USER/.ssh/known_hosts | grep $HOST`" ]; then
 fi
 
 su - $USER -c "mkdir -p stats && mkdir log"
-su - $USER -c "rsync -Prv --rsh 'sshpass -p $REMOTE_PWD ssh -l $REMOTE_USER' $REMOTE_USER@$HOST:/home/$REMOTE_USER/$SUBPATH/var/filestorage/Data.fs ./zope_buildout/var/filestorage/"
-su - $USER -c "rsync -Prv --rsh 'sshpass -p $REMOTE_PWD ssh -l $REMOTE_USER' $REMOTE_USER@$HOST:/home/$REMOTE_USER/$SUBPATH/var/blobstorage ./zope_buildout/var/"
-su - $USER -c "rsync -Prv --rsh 'sshpass -p $REMOTE_PWD ssh -l $REMOTE_USER' $REMOTE_USER@$HOST:/home/$REMOTE_USER/log/* ./log/"
-su - $USER -c "rsync -Prv --rsh 'sshpass -p $REMOTE_PWD ssh -l $REMOTE_USER' $REMOTE_USER@$HOST:/home/$REMOTE_USER/stats/* ./stats/"
+su - $USER -c "rsync -Prv --rsh 'sshpass -p $REMOTE_PWD ssh -l $REMOTE_USER' $REMOTE_USER@$HOST:/home/$REMOTE_ACCOUNT/$SUBPATH/var/filestorage/Data.fs ./zope_buildout/var/filestorage/"
+su - $USER -c "rsync -Prv --rsh 'sshpass -p $REMOTE_PWD ssh -l $REMOTE_USER' $REMOTE_USER@$HOST:/home/$REMOTE_ACCOUNT/$SUBPATH/var/blobstorage ./zope_buildout/var/"
+su - $USER -c "rsync -Prv --rsh 'sshpass -p $REMOTE_PWD ssh -l $REMOTE_USER' $REMOTE_USER@$HOST:/home/$REMOTE_ACCOUNT/log/* ./log/"
+su - $USER -c "rsync -Prv --rsh 'sshpass -p $REMOTE_PWD ssh -l $REMOTE_USER' $REMOTE_USER@$HOST:/home/$REMOTE_ACCOUNT/stats/* ./stats/"
