1deede45c2c110fbae12cd0717204d94edf0af97
Peter Mathis <peter.mathis@kombinat.at>
2020-03-07 14:47:42 +0100

    update cloud agent installation

--- install_ionos_cloud_agent.sh
+++ install_ionos_cloud_agent.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ "$(date +%Y%m%d)" -ge "20200119" ]; then
+if [ "$(date +%Y%m%d)" -ge "20200406" ]; then
     echo "This installation script is out of date"
     echo "Please, download a new script from your Cloud Panel"
     exit
@@ -11,12 +11,12 @@ acronis_file="Backup_Agent_for_Linux_x86_64.bin"
 
 if which zypper >/dev/null 2>&1; then
     zypper refresh -fdb >/dev/null
-    zypper -n in kernel-default kernel-source gcc make perl rpm
+    zypper -n in kernel-default kernel-source gcc make perl rpm >/dev/null
 elif which apt-get >/dev/null 2>&1; then
     apt-get update >/dev/null
-    apt-get -y install linux-image-$(uname -r) linux-headers-$(uname -r) curl gcc make perl rpm
+    apt-get -y install linux-image-$(uname -r) linux-headers-$(uname -r) gcc make perl rpm >/dev/null
 fi
 
 curl -s ${acronis_repo}/${acronis_file} -o /tmp/${acronis_file}
-bash /tmp/${acronis_file}
+bash /tmp/${acronis_file} --auto --token=63FC-53D0-4C36
 rm /tmp/${acronis_file}
