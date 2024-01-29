8f7bd143f6d2be741576537066e65170ac8e0b9e
Peter Mathis <peter.mathis@kombinat.at>
2024-01-29 09:31:54 +0100

    fix package names

--- setup_server.sh
+++ setup_server.sh
@@ -5,7 +5,7 @@ HOME="/home"
 apt-get install -y nano pkg-config bash-completion nginx awstats build-essential ntpdate net-tools rename lynx
 
 # python dependencies
-apt-get install -y libjpeg8-dev libssl-dev libpcre++-dev libpng-dev libxslt1-dev libxml2-dev zlib1g-dev libmemcached-dev libreadline-dev libncurses5-dev libyaml-dev libsqlite3-dev python-docutils poppler-utils libffi-dev
+apt-get install -y libjpeg8-dev libssl-dev libpcre++-dev libpng-dev libxslt1-dev libxml2-dev zlib1g-dev libmemcached-dev libreadline-dev libncurses5-dev libyaml-dev libsqlite3-dev poppler-utils libffi-dev
 apt-get install -y php-fpm
 
 # varnish
